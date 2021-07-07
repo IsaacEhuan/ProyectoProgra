f9dd2df813c746d3618f230b12f5323b470b6464
IsaacEhuan <72111196+IsaacEhuan@users.noreply.github.com>
2021-07-07 15:30:49 -0500

    Update proyect.c

--- proyect.c
+++ proyect.c
@@ -6,9 +6,15 @@
 void imprimirInfo();
 void leerInfo();
 void realizarConsulta();
-//void ordenarConsulta()
+//void ordenarConsulta();
+void copiarEnArreglo();
+void seleccionarDatos();
+void arregloaTXT();
+int contarFilas();
 
-char resp[1];
+char resp[1], c[5000], condicion1[20], condicion2[20], condicion3[20], condicion21[50], q;
+int auxiliar=0, condicion22, contadorTokens=0, limiteInferior=10, filas;
+float condicion23;
 	
 int main(int argc, char *argv[]) {
 	puts ("¿Desea agregar a un alumno a la base de datos? S/N");
@@ -16,15 +22,19 @@ int main(int argc, char *argv[]) {
 	strupr(resp);
 	
 	if (strcmp (resp, "S")==0){
+		system ("cls");
 		leerInfo();
 	}else{
+		system ("cls");
 		puts ("¿Desea realizar una consulta en la base de datos? S/N");
 		gets (resp);
 		strupr(resp);
 		if (strcmp (resp, "S")==0){
+			system ("cls");
 			realizarConsulta();
 			//ordenarConsulta();
 		}else{
+			system ("cls");
 			puts ("No se hizo ninguna consulta :(\nPrograma finalizado con éxito :)");
 		}
 	}
@@ -146,20 +156,213 @@ void leerInfo () {
 }
 
 void realizarConsulta(){
-	char condicion1[20], condicion2[20], condicion3[20], c;
-	int auxiliar=0;
 	
+	seleccionarDatos();
+	
+	filas=contarFilas();
+	
+	for (int k=0; k<filas; k++){
+		copiarEnArreglo();
+		
+		if(condicion1[0]=='*'){
+			int columna;
+			
+			switch (condicion2[4]){
+				case '1':
+					columna=1;
+					break;
+				case '2':
+					columna=2;
+					break;
+				case '3':
+					columna=3;
+					break;
+				case '4':
+					columna=4;
+					break;
+				case '5':
+					columna=5;
+					break;
+				case '6':
+					columna=6;
+					break;
+				case '7':
+					columna=7;
+					break;
+				case '8':
+					columna=8;
+					break;
+				case '9':
+					columna=9;
+					break;
+				default:
+					columna=10;
+					break;
+			}
+			
+			int contadorExtra=1, /*posicion,*/ p=0;
+			while (c[p]!='\0'){
+				if(c[p]=='|'){
+					contadorExtra++;
+					if(contadorExtra==columna){
+						//posicion=p;
+						break;
+					}
+				}
+				p++;
+			}
+			
+			int number=(float)(c[p+2]);
+			if ((c[p+3]>=48) && (c[p+3]<=57)){
+				char nuevo=c[p+2]+c[p+3];
+				number=(float)(nuevo);
+			}
+			
+			switch(condicion2[4]){
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '8':
+				case '0':
+					//Me compara caracteres evaluando sobre (condicion21)
+					
+					break;
+				case '7': //Evaluando enteros sobre el semestre (condicion22).
+					switch (auxiliar){
+						case 1:
+							if((condicion22<number)){
+								arregloaTXT();
+							}
+							break;
+						case 2:
+							if((condicion22>number)){
+								arregloaTXT();
+							}
+							break;
+						case 3:
+							if((condicion22<=number)){
+								arregloaTXT();
+							}
+							break;
+						case 4:
+							if((condicion22>=number)){
+								arregloaTXT();
+							}
+							break;
+						case 5:
+							if((condicion22!=number)){
+								arregloaTXT();
+							}
+							break;
+						case 6:
+							if((condicion22==number)){
+								arregloaTXT();
+							}
+							break;
+					}
+				case '9'://Evaluando flotantes sobre calificaciones (condicion23)
+					switch (auxiliar){
+						case 1:
+							if((condicion23<number)){
+								arregloaTXT();
+							}
+							break;
+						case 2:
+							if((condicion23>number)){
+								arregloaTXT();
+							}
+							break;
+						case 3:
+							if((condicion23<=number)){
+								arregloaTXT();
+							}
+							break;
+						case 4:
+							if((condicion23>=number)){
+								arregloaTXT();
+							}
+							break;
+						case 5:
+							if((condicion23!=number)){
+								arregloaTXT();
+							}
+							break;
+						case 6:
+							if((condicion23==number)){
+								arregloaTXT();
+							}
+							break;
+					}
+			}
+		}else{
+			
+		}
+	}
+	
+	return;
+}
+	
+	
+void seleccionarDatos(){
 	do{
 		printf ("Seleccionar ");
 		gets (condicion1);
-		printf ("donde ");
-		gets (condicion2);
+		printf ("donde (seleccione la columna a comparar y la operación a ejecutar)");
+		gets (condicion2); //Ejemplo Col<>
+		
+		switch (condicion2[4]){
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '8':
+		case '0':
+			printf("Escriba el valor con el que se comparará la columna:");
+			gets(condicion21); //Ejemplo A17000919
+			break;
+		case '7':
+			printf("Escriba el valor con el que se comparará la columna:");
+			scanf ("%d", &condicion22); //Ejemplo 5
+			break;
+		case '9':
+			printf("Escriba el valor con el que se comparará la columna:");
+			scanf ("%f", &condicion23); //Ejemplo 89.34
+			break;
+		}
+		
+		getchar();
+		
 		printf ("ordenado por ");
 		gets (condicion3);
 		
+		switch (condicion2[4]){
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '8':
+		case '0':
+			printf ("Seleccionar %s donde %s%s ordenado por %s\n", condicion1, condicion2, condicion21, condicion3);
+			strupr(condicion21);
+			break;
+		case '7':
+			printf ("Seleccionar %s donde %s%d ordenado por %s\n", condicion1, condicion2, condicion22, condicion3);
+			break;
+		case '9':
+			printf ("Seleccionar %s donde %s%f ordenado por %s\n", condicion1, condicion2, condicion23, condicion3);
+			break;
+		}
+		
+		system ("pause");
 		system ("cls");
 		
-		printf ("Seleccionar %s donde %s ordenado por %s\n", condicion1, condicion2, condicion3);
 		strupr(condicion1);
 		strupr(condicion2);
 		strupr(condicion3);
@@ -190,31 +393,73 @@ void realizarConsulta(){
 			puts("La operacion indicada no es valida, favor de ingresar de nuevo las condiciones");
 		}
 	}while(auxiliar==0);
-	
-	FILE * flujo = fopen("alumnos.txt","r");//Archivo de origen (Base de datos).
-	FILE * flujo2 = fopen("aux.txt","w");//Archivo donde se almacena la consulta.
-	
-	fscanf(flujo,"%c",&c);
-	
-	while(c!=EOF){
-		fprintf(flujo2,"%c",c);
-		fscanf(flujo,"%c",&c);
-		fflush(flujo);
-		fflush(flujo2);
-	}
-	
-	fclose(flujo);
-	fclose(flujo2);
+	return;
+}
+/*void ordenarConsulta(){
 	
 	//FILE * flujo = fopen("consulta","wb");//Archivo final donde se almacena la consulta ordenada.
 	
 	//remove(aux.txt);//Eliminamos el archivo donde e guardó la consulta sin ser ordenado.
+	return;
+}*/
 	
+void copiarEnArreglo(){
+	
+	FILE * flujo = fopen ("alumnos.txt", "r");
+	if (flujo==NULL){
+		perror("No se pudo localizar el archivo");
+		return;
+	}
+	
+	int i=0;
+		
+	while (feof(flujo)==0){
+		q=fgetc(flujo);
+		if(q=='|'){
+			contadorTokens++;
+		}
+		
+		if((contadorTokens>=limiteInferior)&&(contadorTokens<=(limiteInferior+10))){
+			c[i]=q;
+			i++;
+		}
+		
+		if((limiteInferior+10)==contadorTokens){
+			limiteInferior+=10;
+			break;
+		}
+	}
+	
+	fclose(flujo);
 	return;
 }
 	
-/*void ordenarConsulta(){
+void arregloaTXT(){
+	FILE * flujo2 = fopen ("consulta.txt", "a");
+	fputs(c,flujo2);
+	fputs ("\n", flujo2);
+	fclose(flujo2);
+}
+
+int contarFilas(){
+	FILE * flujo = fopen ("alumnos.txt", "r");
+	if (flujo==NULL){
+		perror("No se pudo localizar el archivo");
+		return 10;
+	}
 	
+	int contadorTotal=0, a;
 	
-	return;
-}*/
+	while (feof(flujo)==0){
+		q=fgetc(flujo);
+		if(q=='|'){
+			contadorTotal++;
+		}
+	}
+	
+	a=contadorTotal/10;
+	
+	fclose (flujo);
+	return a;
+}
+
